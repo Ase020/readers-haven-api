9f270d06603d99552a4aca14541d8facf57f6f59
felixNyalenda <felixasejunior@gmail.com>
2023-07-05 00:37:56 +0300

    signup login logout build

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,27 @@ class UsersController < ApplicationController
     users = User.all
     render json: users, status: :ok
   end
+  def create
+    user = User.create!(user_params)
+    if user.valid?
+      render json: user, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: {error: "Not Authorized"}, status: :unauthorized
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:first_name, :last_name, :email, :image_url, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   resources :author_publishers
   resources :authors
   resources :publishers
-  resources :users
+  resources :users, only: [:index]
   resources :books do
   resources :reviews
   end
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post '/login', to: "sessions#show"
+  delete 'logout', to: 'sessions#destroy'
 
 end
