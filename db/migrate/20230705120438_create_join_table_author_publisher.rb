2e331c1f62f44c35df9cab6621290c0d9ac3e3c1
felixNyalenda <felixasejunior@gmail.com>
2023-07-05 16:22:28 +0300

    bug fix

--- db/migrate/20230705120438_create_join_table_author_publisher.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableAuthorPublisher < ActiveRecord::Migration[7.0]
-  def change
-    create_join_table :authors, :publishers do |t|
-      # t.index [:author_id, :publisher_id]
-      # t.index [:publisher_id, :author_id]
-    end
-  end
-end
