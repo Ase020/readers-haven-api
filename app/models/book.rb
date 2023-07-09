b8971932a4383dbe0432f7653f07c6db609d00d4
felixNyalenda <felixasejunior@gmail.com>
2023-07-09 12:03:36 +0300

    add validation to genre

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
 
   # validations
   validates :title, presence:true
-  validates :genre, presence: true
+  validates :genre, presence: true, inclusion: { in: (["Fiction", "Non-fiction", "Mystery", "Thriller", "Romance", "Sci-Fi", "Fantasy", "Historical Fiction", "Biography", "Autobiography", "Self-help", "Business""History", "Travel", "Poetry", "Drama", "Horror", "Comedy", "Children's", "Young Adult"])}
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :publish_date, presence: true
