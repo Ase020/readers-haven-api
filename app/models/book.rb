bb1b25775b3de793ba39770017c6709aff045432
felixNyalenda <felixasejunior@gmail.com>
2023-07-09 12:08:45 +0300

    add dependent destroy to book review

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   belongs_to :author
   belongs_to :publisher
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   # validations
   validates :title, presence:true
